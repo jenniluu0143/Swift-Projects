651f51ec9a9c93b7379e042e4f71372b581347d5
jenniluu0143 <jenniferluu143@gmail.com>
2024-06-04 20:11:56 -0500

    Add files via upload

---
+++ AboutMe/AboutMe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AboutMe
+//
+//  Created by student on 12/8/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
